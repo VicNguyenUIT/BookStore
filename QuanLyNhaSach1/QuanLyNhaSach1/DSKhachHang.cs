3c10a36089fe8e1e7dff87d0894a817bcafaa114
TanKhoa98 <khoapham123vn@gmail.com>
2019-06-29 17:54:34 +0700

    Giao diện + Form Đăng nhập
    Giao diện + Form Đăng nhập

---
+++ QuanLyNhaSach1/QuanLyNhaSach1/DSKhachHang.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyNhaSach1
+{
+    class DSKhachHang
+    {
+        public KhachHang KhachHang { get; set; }
+        public int STT { get; set; }
+    }
+}
